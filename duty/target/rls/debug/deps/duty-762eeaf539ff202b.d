28d51e15e1d3e1e5531b4b9bce872c4967468978
Niklas Lakner <niklas.lakner@gmail.com>
2021-05-16 12:53:47 +0000

    project init

---
+++ duty/target/rls/debug/deps/duty-762eeaf539ff202b.d
@@ -0,0 +1,5 @@
+/workspace/system/duty/target/rls/debug/deps/duty-762eeaf539ff202b.rmeta: duty/src/lib.rs
+
+/workspace/system/duty/target/rls/debug/deps/duty-762eeaf539ff202b.d: duty/src/lib.rs
+
+duty/src/lib.rs:
---
+++ rest/target/rls/debug/deps/rest-bf93ccc6d2dd0e9d.d
@@ -0,0 +1,5 @@
+/workspace/system/rest/target/rls/debug/deps/rest-bf93ccc6d2dd0e9d.rmeta: rest/src/main.rs
+
+/workspace/system/rest/target/rls/debug/deps/rest-bf93ccc6d2dd0e9d.d: rest/src/main.rs
+
+rest/src/main.rs:
